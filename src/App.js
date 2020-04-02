ae6a741675278c872fe75914e58f1720c19cc5c4
Matvei Bohush <bohush.mathew@gmail.com>
2020-04-02 23:39:24 +0300

    lesson 33 features

--- src/App.js
+++ src/App.js
@@ -4,28 +4,29 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import { Route } from "react-router-dom";
+import {Route} from "react-router-dom";
+import { addPost } from "./redux/state";
 
 const App = props => {
-  return (
-      <div className="app-wrapper">
-        <Header />
-        <Navbar />
-        <div className="app-wrapper-content">
-          <Route
-            path="/Profile"
-            render={() => 
-            <Profile state={props.state.profilePage} />}
-          />
-          <Route
-            path="/Dialogs"
-            render={() => (
-              <Dialogs state={props.state.dialogsPage} />
-            )}
-          />
+    return (
+        <div className="app-wrapper">
+            <Header/>
+            <Navbar/>
+            <div className="app-wrapper-content">
+                <Route
+                    path="/Profile"
+                    render={() =>
+                        <Profile state={props.state.profilePage} addPost={props.addPost} />}
+                />
+                <Route
+                    path="/Dialogs"
+                    render={() => (
+                        <Dialogs state={props.state.dialogsPage}/>
+                    )}
+                />
+            </div>
         </div>
-      </div>
-  );
+    );
 };
 
 export default App;
